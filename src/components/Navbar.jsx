b62a45a2bb59753b84bb3fa092848b08b26893d0
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-03-01 16:14:06 +0000

    Add Navbar component with logo and create initial Footer component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <>
+        <div>
+            <img src="./assets/Navbar_logo" alt="website logo" />
+        </div>
+        <nav></nav>
+    </>
+  );
+};
+
+export default Navbar;
