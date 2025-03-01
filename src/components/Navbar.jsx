e30c0da8ac4c9d0233ede889f788cc91a13d19ea
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-03-01 17:30:06 +0000

    Refactor Navbar component to use react-router for navigation and update logo implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
+import { Link } from 'react-router-dom';
+import '../styles/components/Navbar.css';
+import NavbarLogo from '../assets/Navbar_logo.svg';
+
 const Navbar = () => {
   return (
-    <>
-        <div>
-            <img src="./assets/Navbar_logo" alt="website logo" />
+    <header className="navbar">
+        <div className="navbar-logo">
+            <Link to="/">
+                <img src={NavbarLogo} alt="website logo" />
+            </Link>
         </div>
-        <nav></nav>
-    </>
+        <nav>
+              <ul className="nav-links">
+                  <li>
+                      <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link to="/convert">Translate</Link>
+                  </li>
+                  <li>
+                      <Link to="/contact">Contact</Link>
+                  </li>
+              </ul>
+        </nav>
+    </header>
   );
 };
 
