556954a97f1adf4b83e2bb7ab501efc53b062688
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-03-05 20:35:51 +0000

    Wrap App component with AppProvider for context management and update MorseCode comments for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ import Contact from './pages/contact';
 import Convert from './pages/convert';
 import NotFound from './pages/Notfound';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import { AppProvider } from './Context/AppContext';
 import './styles/App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/convert" element={<Convert />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AppProvider>
+      <Router>
+        <div className="app-container">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/convert" element={<Convert />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AppProvider>
   );
 }
 
